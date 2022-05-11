31af217c4b3e312573c57f36be8e5515544fe132
daisuke kitsunai <daisuke_kitsunai@myway.co.jp>
2022-05-11 17:49:12 +0900

    ざっくりHost

--- killmewpf/App.xaml.cs
+++ killmewpf/App.xaml.cs
@@ -1,8 +1,17 @@
 ﻿using killmewpf.View;
 using killmewpf.ViewModel;
+using System.Reactive.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
+using System.Reactive.Disposables;
 using System.Windows;
 using System.Windows.Data;
+using ZLogger;
+using Reactive.Bindings.Extensions;
 
 namespace killmewpf
 {
@@ -11,14 +20,77 @@ namespace killmewpf
     /// </summary>
     public partial class App : Application
     {
+        private readonly CompositeDisposable disposables = new();
+        private IHost host;
+        private ILogger<App> logger;
+
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            host = BootStrap(e.Args);
+            host.AddTo(disposables);
+
+            logger = host.Services.GetRequiredService<ILogger<App>>();
+            logger.ZLogInformation("app boot");
+            // base.OnStartup(e);
+
+            host.Start();
+
             // 直接コードで登録する方法もある
             this.Resources.RegistorDataTemplate<SubView1, SubViewModel1>();
 
-            this.MainWindow = new MainWindow();
+            this.MainWindow = host.Services.GetRequiredService<MainWindow>();
             this.MainWindow.Show();
         }
+
+        private IHost BootStrap(string[] args)
+        {
+            var appConst = new AppConst();
+            var logDir = new DirectoryInfo(appConst.LogDirName);
+            logDir.Create();
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, configure) =>
+                {
+                    configure.SetBasePath(context.HostingEnvironment.ContentRootPath);
+                    configure.AddJsonFile(appConst.ConfigFileName, true, true);
+                    configure.AddCommandLine(args);
+                })
+                .ConfigureServices((context, service) =>
+                {
+                    service.AddSingleton(appConst);
+                    service.Configure<AppConfig>(context.Configuration);
+
+                    service.AddSingleton<MainWindow>();
+                })
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+
+                    logging.AddZLoggerRollingFile(
+                        (dt, x) => $"{appConst.LogDirName}/{dt.ToLocalTime():yyyy-MM-dd}_{x:000}.log",
+                        x => x.ToLocalTime().Date,
+                        1024 * 1024);
+
+                    logging.AddDebug();
+                })
+                .Build();
+
+
+            return host;
+        }
+
+        private async void Application_Exit(object sender, ExitEventArgs e)
+        {
+            logger.ZLogInformation("app exit");
+            await host.StopAsync(TimeSpan.FromSeconds(5));
+            disposables.Dispose();
+            // base.OnExit(e);
+        }
+    }
+    internal class AppConfig
+    {
     }
 
     public static class IViewForExtensions
@@ -38,5 +110,13 @@ namespace killmewpf
 
             resourceDictionary.Add(dt.DataTemplateKey, dt);
         }
+    }
+
+
+    public class AppConst
+    {
+        public string LogDirName => "Log";
+
+        public string ConfigFileName => "hostConfig.json";
     }
 }
